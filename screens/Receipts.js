a2fb05d3af677f889538e7bb76d6a737bb550785
s5227111 <s5227111@bournemouth.ac.uk>
2022-11-25 23:10:46 +0000

    Google Vision AI

--- screens/Receipts.js
+++ screens/Receipts.js
@@ -1,7 +1,13 @@
-import { Text } from "react-native";
+import { View } from "react-native";
+import ImagePickerComponent from "../components/ImagePickerComponent";
+import callGoogleVisionAsync from "../helperFunctions.js";
 
 const Receipts = () => {
-  return <Text>Receipts Screen</Text>;
+  return (
+    <View style={{ marginTop: 300 }}>
+      <ImagePickerComponent onSubmit={callGoogleVisionAsync} />
+    </View>
+  );
 };
 
 export default Receipts;
